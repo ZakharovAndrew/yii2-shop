43a744489d542543bea72e38bcd7378901c30824
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-08 22:42:51 +0300

    Add files via upload

--- src/views/product/index.php
+++ src/views/product/index.php
@@ -1,16 +1,17 @@
 <?php
 
-use app\models\Product;
+use ZakharovAndrew\shop\models\Product;
+use ZakharovAndrew\shop\Module;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
 
 /** @var yii\web\View $this */
-/** @var app\models\ProductSearch $searchModel */
+/** @var ZakharovAndrew\shop\models\ProductSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Products';
+$this->title = Module::t('Products');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-index">
@@ -30,9 +31,22 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'title',
-            'description:ntext',
-            'url:url',
+            'title',           
+            [
+                'attribute' => 'description',
+                'format' => 'raw',
+
+                'content' => function ($model) {
+                    return (mb_substr(strip_tags($model->description), 0, 50)).'...';
+                }
+            ],
+            [
+                'attribute' => 'url',
+                'format' => 'raw',
+                'content' => function ($model) {
+                    return '<a href="'.Url::toRoute(['view-by-url', 'url' => $model->url]).'">'.$model->url.'</a>';
+                }
+            ],
             'images',
             //'category_id',
             //'user_id',
