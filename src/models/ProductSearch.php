426a34b273bdcef63f2be4a485fef3054daecaae
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-08 22:52:49 +0300

    Add files via upload

--- src/models/ProductSearch.php
+++ src/models/ProductSearch.php
@@ -72,4 +72,46 @@ class ProductSearch extends Product
 
         return $dataProvider;
     }
+    
+    /***
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function searchCatalog($params)
+    {
+        $query = Product::find();
+        
+         // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            /*'category_id' => $this->category_id,
+            'user_id' => $this->user_id,
+            'count_views' => $this->count_views,
+            'created_at' => $this->created_at,*/
+        ]);
+
+        /*$query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'url', $this->url])
+            ->andFilterWhere(['like', 'images', $this->images]);*/
+
+        return $dataProvider;
+    }
 }
