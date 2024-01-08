79f2d649bd1f7abe58dab956ebec1be80eeb87f0
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-08 22:36:57 +0300

    Add files via upload

---
+++ src/views/catalog/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use ZakharovAndrew\shop\models\Product;
+//use ZakharovAndrew\shop\Module;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\shop\models\ProductSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$module = Yii::$app->getModule('shop');
+//var_dump($module->catalogTitle);die();
+$this->title = $module->catalogTitle;
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="catalog-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+</div>
