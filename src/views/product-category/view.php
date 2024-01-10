a1e6711d71a6643583de74611ce00f7b839e7acd
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-11 01:37:34 +0300

    Add files via upload

--- src/views/product-category/view.php
+++ src/views/product-category/view.php
@@ -21,13 +21,57 @@ foreach (ProductCategory::getCategories($model->id) as $category) {
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
+<style>
+    .shop-product-img img {
+        width:100%;
+        border-radius: 9px 9px 0 0;
+    }
+    .product-price {
+        font-size:24px;
+        text-align: center;
+    }
+    .shop-product-item {
+        border: 1px solid #cccccc;
+        border-radius: 9px;
+        box-sizing: border-box;
+        margin-bottom:20px;
+    }
+    .product-title {
+        text-align: center;
+        font-size:20px;
+    }
+    .to-album {
+        margin:10px 0;
+        text-align: center;
+    }
+    .to-album button {
+        
+        border-radius: 20px;
+        background-color: #FFC42E;
+        border:none;
+        padding:10px 25px;
+    }
+    .category-description {margin: 30px 0}
+    
+    
+</style>
 <div class="product-category-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?php if (!Yii::$app->user->isGuest) {?>
     <p>
         <?= Html::a(Module::t('Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
+    <?php } ?>
+    
+    <div class="row product-list">
+        <?php foreach($products as $product) {
+            echo $this->render('../catalog/_product', [
+                'model' => $product,
+                'class' => 'col-md-4 col-12 shop-product'
+            ]);                
+        } ?>
+    </div>
     
     <div class="category-description"><?= $model->description ?></div>
     <div class="category-description_after"><?= $model->description_after ?></div>
