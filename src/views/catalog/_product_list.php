74ff52aeb1231c8393d348be582fe631c5e0b32f
Zakharov Andrey <prostonado_07@mail.ru>
2025-03-25 00:03:59 +0300

    updated style

--- src/views/catalog/_product_list.php
+++ src/views/catalog/_product_list.php
@@ -16,20 +16,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
 ?>
 
 <style>
-    .shop-product-img img {
-        width:100%;
-        border-radius: 9px 9px 0 0;
-    }
-    .product-price {
-        font-size:24px;
-        text-align: center;
-    }
-    .shop-product-item {
-        border: 1px solid #cccccc;
-        border-radius: 9px;
-        box-sizing: border-box;
-        margin-bottom:20px;
-    }
+    
     .product-title {
         text-align: center;
         padding: 0 5px;
