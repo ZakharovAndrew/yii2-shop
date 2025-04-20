76552dc6fe43e7b2c1e8625acfc41e52c615dc98
Zakharov Andrey <prostonado_07@mail.ru>
2025-04-20 15:07:33 +0300

    Bulk pricing

--- src/messages/ru/shop.php
+++ src/messages/ru/shop.php
@@ -60,6 +60,8 @@ return [
     'Bulk quantity 3' => 'Количество для опта 3',
     'Bulk price 3' => 'Оптовая цена 3',
     'Set prices for different quantity ranges' => 'Установите цены для разных диапазонов количества',
+    'Quantity threshold' => 'Порог количества',
+    'Special price' => 'Спеццена',
     //category
     'Product Categories' => 'Категории товаров',
     'Create Product Category' => 'Создать категорию товаров',
