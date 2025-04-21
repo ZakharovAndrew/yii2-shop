aa32a0413c78d805a560bb3fd983038bc4b9506d
Zakharov Andrey <prostonado_07@mail.ru>
2025-04-21 13:02:51 +0300

    fixed cart

--- src/assets/ShopAssets.php
+++ src/assets/ShopAssets.php
@@ -13,7 +13,7 @@ class ShopAssets extends AssetBundle
     public $sourcePath = '@vendor/zakharov-andrew/yii2-shop/src/assets';
 
     public $css = [
-        'css/style_v1.11.css',
+        'css/style_v1.12.css',
     ];
 
     public $js = [
