63e46b0a503da5b4cfee60fc6ba4399db4102a3c
Zakharov Andrey <prostonado_07@mail.ru>
2025-04-29 08:59:13 +0300

    added composition and weight

---
+++ migrations/m240107_180018_add_columns_to_product_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m240107_180018_add_columns_to_product_table extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('product', 'composition', $this->text());
+        $this->addColumn('product', 'weight', $this->decimal(10, 2)->notNull());
+    }
+
+    public function down()
+    {
+        $this->dropColumn('product', 'composition');
+        $this->dropColumn('product', 'weight');
+    }
+}
