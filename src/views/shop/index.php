880ff19fcf709b5af6e09305cad9ce7014fd497b
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-09-01 22:57:51 +0300

    Create index.php

---
+++ src/views/shop/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use ZakharovAndrew\shop\models\Shop;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use ZakharovAndrew\shop\Module;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\shop\models\ShopSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Shops';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="shop-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Module::t('Create Shop'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'description:ntext',
+            'url:url',
+            'avatar',
+            'created_at',
+            //'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Shop $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
