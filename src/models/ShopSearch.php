96c6185d2e0f148a9955c22ec25509f4935b9877
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-09-01 22:45:06 +0300

    Create ShopSearch.php

---
+++ src/models/ShopSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ZakharovAndrew\shop\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use ZakharovAndrew\shop\models\Shop;
+
+/**
+ * ShopSearch represents the model behind the search form of `app\models\Shop`.
+ */
+class ShopSearch extends Shop
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['name', 'description', 'url', 'avatar', 'created_at', 'updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     * @param string|null $formName Form name to be used into `->load()` method.
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params, $formName = null)
+    {
+        $query = Shop::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params, $formName);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'url', $this->url])
+            ->andFilterWhere(['like', 'avatar', $this->avatar]);
+
+        return $dataProvider;
+    }
+}
