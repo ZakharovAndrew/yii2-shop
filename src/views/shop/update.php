5a47e588a8cba79bb85a6e2b25a4aa406445b16b
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-09-03 23:25:25 +0300

    Create update.php

---
+++ src/views/shop/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\shop\models\Shop $model */
+
+$this->title = 'Update Shop: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Shops', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="shop-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
