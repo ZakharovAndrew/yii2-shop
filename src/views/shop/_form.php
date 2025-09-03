e315e897df73cb890fe991e8cb08727ae79a8080
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-09-03 23:28:20 +0300

    Create _form.php

---
+++ src/views/shop/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\shop\models\Shoph $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="shop-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'avatar')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
