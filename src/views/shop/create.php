f73da424fe813f4559c94984db9f71d9a453b8da
Zakharov Andrey <prostonado_07@mail.ru>
2025-09-04 00:04:59 +0300

    fixed

--- src/views/shop/create.php
+++ src/views/shop/create.php
@@ -7,7 +7,7 @@ use ZakharovAndrew\shop\Module;
 /** @var ZakharovAndrew\shop\models\Shop $model */
 
 $this->title = Module::t('Create Store');
-$this->params['breadcrumbs'][] = ['label' => Module::t('Stores '), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Module::t('Stores'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="shop-create">
