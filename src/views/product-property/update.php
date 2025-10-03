295bc8f28e63513408b39e70a37295358af9ae01
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-03 23:58:48 +0300

    fixed

--- src/views/product-property/update.php
+++ src/views/product-property/update.php
@@ -8,26 +8,14 @@ use ZakharovAndrew\shop\Module;
 
 $this->title = Module::t('Update Product Property: {name}', ['name' => $model->name]);
 $this->params['breadcrumbs'][] = ['label' => Module::t('Product Properties'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Module::t('Update');
 ?>
 <div class="product-property-update">
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
-                </div>
-                <div class="box-body">
+    <?php if (Yii::$app->getModule('shop')->showTitle) {?><h3><?= Html::encode($this->title) ?></h3><?php } ?>
 
-                    <?= $this->render('_form', [
-                        'model' => $model,
-                    ]) ?>
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
 
 </div>
