03fb473587634411ff4bbfd0addcb43387caae99
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-04 23:57:47 +0300

    Add product catalog styles

--- src/assets/ShopAssets.php
+++ src/assets/ShopAssets.php
@@ -13,7 +13,7 @@ class ShopAssets extends AssetBundle
     public $sourcePath = '@vendor/zakharov-andrew/yii2-shop/src/assets';
 
     public $css = [
-        'css/style_v1.126.css',
+        'css/style_v1.131.css',
     ];
 
     public $js = [
--- src/views/product-category/view.php
+++ src/views/product-category/view.php
@@ -86,87 +86,91 @@ $this->params['breadcrumbs'][] = $model->title;
     <?php } ?>
     
     <div class="category-description"><?= $model->description ?></div>
+</div>
+<div class="products-catalog">
+    <div class="products-catalog__left">
+        <!-- Фильтр по цветам -->
+        <?php if (!empty($availableColors)): ?>
+            <div class="color-filter mb-4">
+                <h4><?= Module::t('Filter by Color') ?></h4>
 
-    <!-- Фильтр по цветам -->
-    <?php if (!empty($availableColors)): ?>
-        <div class="color-filter mb-4">
-            <h4><?= Module::t('Filter by Color') ?></h4>
-            
-            <div class="color-filter-wrapper">
-                <div class="color-filter-options">
-                    <a href="<?= Url::to(['/shop/product-category/view', 'url' => $model->url]) ?>" 
-                       class="color-filter-btn <?= empty($selectedColors) ? 'active' : '' ?>">
-                        <?= Module::t('All Colors') ?>
-                    </a>
-                    
-                    <!-- Colors options  -->
-                    <?php foreach ($availableColors as $color): ?>
-                        <?php
-                        $isActive = in_array($color->id, $selectedColors);
-                        $urlParams = ['/shop/product-category/view', 'url' => $model->url];
-                        
-                        if ($isActive) {
-                            // Удаляем цвет из фильтра
-                            $newColors = array_diff($selectedColors, [$color->id]);
-                        } else {
-                            // Добавляем цвет в фильтр
-                            $newColors = array_merge($selectedColors, [$color->id]);
-                        }
-                        
-                        if (!empty($newColors)) {
-                            $urlParams['colors'] = $newColors;
-                        }
-                        ?>
-                        <a href="<?= Url::to($urlParams) ?>" 
-                           class="color-filter-option <?= $isActive ? 'active' : '' ?>"
-                           title="<?= Html::encode($color->name) ?>"
-                           style="background-color: <?= $color->css_color ?>">
+                <div class="color-filter-wrapper">
+                    <div class="color-filter-options">
+                        <a href="<?= Url::to(['/shop/product-category/view', 'url' => $model->url]) ?>" 
+                           class="color-filter-btn <?= empty($selectedColors) ? 'active' : '' ?>">
+                            <?= Module::t('All Colors') ?>
                         </a>
-                    <?php endforeach; ?>
-                    <?php if (!empty($selectedColors)): ?>
-                    
-                        <a href="<?= Url::to(['/shop/product-category/view', 'url' => $model->url]) ?>" class="clear-filter-link">
-                            <?= Module::t('Clear filter') ?>
-                        </a>
-                    <?php endif; ?>
+
+                        <!-- Colors options  -->
+                        <?php foreach ($availableColors as $color): ?>
+                            <?php
+                            $isActive = in_array($color->id, $selectedColors);
+                            $urlParams = ['/shop/product-category/view', 'url' => $model->url];
+
+                            if ($isActive) {
+                                // Удаляем цвет из фильтра
+                                $newColors = array_diff($selectedColors, [$color->id]);
+                            } else {
+                                // Добавляем цвет в фильтр
+                                $newColors = array_merge($selectedColors, [$color->id]);
+                            }
+
+                            if (!empty($newColors)) {
+                                $urlParams['colors'] = $newColors;
+                            }
+                            ?>
+                            <a href="<?= Url::to($urlParams) ?>" 
+                               class="color-filter-option <?= $isActive ? 'active' : '' ?>"
+                               title="<?= Html::encode($color->name) ?>"
+                               style="background-color: <?= $color->css_color ?>">
+                            </a>
+                        <?php endforeach; ?>
+                        <?php if (!empty($selectedColors)): ?>
+
+                            <a href="<?= Url::to(['/shop/product-category/view', 'url' => $model->url]) ?>" class="clear-filter-link">
+                                <?= Module::t('Clear filter') ?>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+
+
                 </div>
-                
-                
             </div>
-        </div>
-    <?php endif; ?>
-    
-    <div class="products-header">
-        <div class="selected-filters"></div>
-        <?php $form = ActiveForm::begin([
-            'method' => 'get',
-            'action' => ['/shop/product-category/view', 'url' => $url, 'colors' => $colors], // явно укажите действие
-        ]); ?>
-            <?= Html::dropDownList(
-                'sorting',
-                $sorting,
-                ProductCategory::sortingLabels(),
-                [
-                    'class' => 'form-control form-select',
-                    'onchange' => 'this.form.submit()'
-                ]
-            ) ?>
-        <?php ActiveForm::end(); ?>
+        <?php endif; ?>
     </div>
+    <div class="products-catalog__right">
+        <div class="products-header">
+            <div class="selected-filters"></div>
+            <?php $form = ActiveForm::begin([
+                'method' => 'get',
+                'action' => ['/shop/product-category/view', 'url' => $url, 'colors' => $colors], // явно укажите действие
+            ]); ?>
+                <?= Html::dropDownList(
+                    'sorting',
+                    $sorting,
+                    ProductCategory::sortingLabels(),
+                    [
+                        'class' => 'form-control form-select',
+                        'onchange' => 'this.form.submit()'
+                    ]
+                ) ?>
+            <?php ActiveForm::end(); ?>
+        </div>
     
-    <?= $this->render('../catalog/_product_list', [
-        'products' => $products,
-        'pagination' => $pagination,
-        'class' => (Yii::$app->getModule('shop')->mobileProductsPerRow == 2 ? 'col-md-4 col-6 shop-product' : 'col-md-4 col-12 shop-product')
-    ]) ?>
+        <?= $this->render('../catalog/_product_list', [
+            'products' => $products,
+            'pagination' => $pagination,
+            'class' => (Yii::$app->getModule('shop')->mobileProductsPerRow == 2 ? 'col-md-4 col-6 shop-product' : 'col-md-4 col-12 shop-product')
+        ]) ?>
         
-    <div class="category-description_after"><?= $model->description_after ?></div>
+        <div class="category-description_after"><?= $model->description_after ?></div>
     
-    <?php if (!Yii::$app->user->isGuest) {?>
-    <p>
-        <?= Html::a(Module::t('Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-    </p>
-    <?php } ?>
+        <?php if (!Yii::$app->user->isGuest) {?>
+        <p>
+            <?= Html::a(Module::t('Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        </p>
+        <?php } ?>
+    </div>
 </div>
 <style>
     .products-header {
