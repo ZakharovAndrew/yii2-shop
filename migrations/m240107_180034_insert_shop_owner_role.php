d58bf0288fcb088d452995740e22270dfbcf8165
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-08 00:54:48 +0300

    add shop owner role migration

---
+++ migrations/m240107_180034_insert_shop_owner_role.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+use ZakharovAndrew\shop\Module;
+
+/**
+ * Class m240107_180034_insert_shop_owner_role
+ */
+class m240107_180034_insert_shop_owner_role extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('roles', [
+            'title' => Module::t('Shop Owner'),
+            'code' => 'shop_owner',
+            'description' => Module::t('Shop owner role with full access rights'),
+            'function_to_get_all_subjects' => 'ZakharovAndrew\shop\models\Shop::getShopsList',
+            'parameters' => '{"2001":"index,update,view"}'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('roles', ['code' => 'shop_owner']);
+    }
+}
