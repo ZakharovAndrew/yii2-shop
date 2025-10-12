2a2fe02295535d1adb3eab4f4a589e94f1c98d47
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-13 00:38:51 +0300

    fixed

--- src/views/catalog/_product_list.php
+++ src/views/catalog/_product_list.php
@@ -5,37 +5,15 @@ use ZakharovAndrew\shop\assets\ShopAssets;
 ShopAssets::register($this);
 
 $script = <<< JS
-        
 $(".add-to-cart").on('click', function () {
     let id = $(this).data('id');
     shop.addCart(id)  
 });
-    
 JS;
 
 $this->registerJs($script, yii\web\View::POS_READY);
 ?>
 
-<style>
-    .product-title {
-        text-align: center;
-        padding: 0 5px;
-        font-size:20px;
-    }
-    .to-album {
-        margin:10px 0;
-        text-align: center;
-    }
-    .to-album button {
-        border-radius: 20px;
-        background-color: #FFC42E;
-        border:none;        
-        padding: 9px 25px;
-        font-family: 'Roboto Condensed', sans-serif;
-    }
-    .category-description {margin: 30px 0}
-    
-</style>
 <div class="row product-list">
     <?php foreach($products as $product) {
         echo $this->render('../catalog/_product', [
