758a8c5b490cfcae0f5c173af1228e6fe0866b78
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-19 01:34:34 +0300

    feat: replace inline SVG with CSS icon system

--- src/assets/ShopAssets.php
+++ src/assets/ShopAssets.php
@@ -13,7 +13,7 @@ class ShopAssets extends AssetBundle
     public $sourcePath = '@vendor/zakharov-andrew/yii2-shop/src/assets';
 
     public $css = [
-        'css/style_v1.159.css',
+        'css/style_v1.169.css',
     ];
 
     public $js = [
--- src/views/product-property/index.php
+++ src/views/product-property/index.php
@@ -75,7 +75,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'up' => function($url, $model, $key) {
                         if ($model->position > 1) {
                             return Html::a(
-                                '<svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 4L4 8M8 4L12 8M8 4V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+                                '<span class="icon-arrow-up"></span>',
                                 ['move-up', 'id' => $model->id],
                                 [
                                     'title' => Module::t('Move up'),
@@ -93,7 +93,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         $maxPosition = \ZakharovAndrew\shop\models\ProductProperty::find()->max('position');
                         if ($model->position < $maxPosition) {
                             return Html::a(
-                                '<svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 12L4 8M8 12L12 8M8 12V4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+                                '<span class="icon-arrow-down"></span>',
                                 ['move-down', 'id' => $model->id],
                                 [
                                     'title' => Module::t('Move down'),
