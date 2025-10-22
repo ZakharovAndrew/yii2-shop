3defaaf82bd96a25b2616a7f4a36893e19ad20a6
Zakharov Andrey <prostonado_07@mail.ru>
2025-10-23 01:23:47 +0300

    added style for dashboards

--- src/assets/ShopAssets.php
+++ src/assets/ShopAssets.php
@@ -13,7 +13,7 @@ class ShopAssets extends AssetBundle
     public $sourcePath = '@vendor/zakharov-andrew/yii2-shop/src/assets';
 
     public $css = [
-        'css/style_v1.169.css',
+        'css/style_v1.180.css',
     ];
 
     public $js = [
